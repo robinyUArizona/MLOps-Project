96d0d6af8fb8c947de3533cea82c856c560c182c
robinyUArizona <robiny@arizona.edu>
2024-02-11 05:36:21 +0545

    Updating logger and exception

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -3,13 +3,13 @@ import sys
 
 class customexception(Exception):
 
-    def __init__(self,error_message,error_details:sys):
-        self.error_message=error_message
-        _,_,exc_tb=error_details.exc_info()
-        print(exc_tb)
+    def __init__(self, error_message, error_details:sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+        # print(exc_tb)
 
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -18,8 +18,7 @@ class customexception(Exception):
 
 if __name__=="__main__":
     try:
-        a=1/0
-
+        a = 1/0
     except Exception as e:
         #print(e)
-        raise customexception(e,sys)
+        raise customexception(e, sys)
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
+
+# import sys
+
+# try:
+#     a = 1/0
+# except Exception as e:
+#     print(e)
+# print(sys.exc_info())
+
 from src.logger.logging import logging
 
-logging.info("this is myb testing")
+logging.info("this is my testing")
+logging.info(" this my second testing")
+# logging.info(" this my second testing")
 
-
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
-
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
+# logging.info(" this my second testing")
+# logging.info(" this my second testing")
+# logging.info(" this my second testing")
