45b9d254db6f0d3cb36b65eecd2dd5d1e3cb3446
robinyUArizona <robiny@arizona.edu>
2024-02-12 09:52:56 +0545

    Added all codes after model training

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -10,17 +10,14 @@ from src.components.model_trainer import ModelTrainer
 from src.components.model_evaluation import ModelEvaluation
 
 
-obj=DataIngestion()
+obj = DataIngestion()
+train_data_path, test_data_path = obj.initiate_data_ingestion()
 
-train_data_path,test_data_path=obj.initiate_data_ingestion()
+data_transformation = DataTransformation()
+train_arr, test_arr = data_transformation.initialize_data_transformation(train_data_path, test_data_path)
 
-data_transformation=DataTransformation()
+model_trainer_obj = ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr, test_arr)
 
-train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
-
-
-model_trainer_obj=ModelTrainer()
-model_trainer_obj.initate_model_training(train_arr,test_arr)
-
-model_eval_obj = ModelEvaluation()
-model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+# model_eval_obj = ModelEvaluation()
+# model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
